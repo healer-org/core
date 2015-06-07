90805477ae66abcf5933e5cc03827a2cf3136e0b
Señor Sisk <jason@sisk.org>
2015-06-07 07:46:47 -0500

    Alphabetize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,5 @@
 Rails.application.routes.draw do
   namespace :v1 do
-    get     "patients/search" => "patients#search"
-    get     "patients"        => "patients#index"
-    get     "patients/:id"    => "patients#show"
-    post    "patients"        => "patients#create"
-    put     "patients/:id"    => "patients#update"
-    delete  "patients/:id"    => "patients#delete"
-
-    get     "cases"     => "cases#index"
-    get     "cases/:id" => "cases#show"
-    post    "cases"     => "cases#create"
-    put     "cases/:id" => "cases#update"
-    delete  "cases/:id" => "cases#delete"
-
     get     "appointments"     => "appointments#index"
     get     "appointments/:id" => "appointments#show"
     post    "appointments"     => "appointments#create"
@@ -20,5 +7,18 @@ Rails.application.routes.draw do
     delete  "appointments/:id" => "appointments#delete"
 
     post    "attachments" => "attachments#create"
+
+    get     "cases"     => "cases#index"
+    get     "cases/:id" => "cases#show"
+    post    "cases"     => "cases#create"
+    put     "cases/:id" => "cases#update"
+    delete  "cases/:id" => "cases#delete"
+
+    get     "patients/search" => "patients#search"
+    get     "patients"        => "patients#index"
+    get     "patients/:id"    => "patients#show"
+    post    "patients"        => "patients#create"
+    put     "patients/:id"    => "patients#update"
+    delete  "patients/:id"    => "patients#delete"
   end
 end
